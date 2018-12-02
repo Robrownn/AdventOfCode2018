fdefc8cf084bf2f03c397512ee64b9436b62425f
Robert Brown <robrownn@gmail.com>
2018-12-02 01:10:36 -0500

    Completed Day 2 Part 1

---
+++ day-2/src/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"./input.txt");
+
+            List<string> twoLetters = SelectDuplicates(lines, (x => x.Count() == 2));
+            List<string> threeLetters = SelectDuplicates(lines, (x => x.Count() == 3));
+            var both = from s1 in twoLetters
+            join s2 in threeLetters on s1 equals s2
+            select s2;
+
+            var checksum = twoLetters.Count() * threeLetters.Count();
+            Console.WriteLine(checksum);
+        }
+
+        static List<string> SelectDuplicates(string[] lines, Func<IGrouping<char, char>, bool> predicate)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string line in lines)
+            {
+                var wtf = line.GroupBy(x => x)
+                    .Where(predicate)
+                    .Any();
+
+                if (wtf)
+                    result.Add(line);
+
+            }
+
+            return result;
+        }
+    }
+}
